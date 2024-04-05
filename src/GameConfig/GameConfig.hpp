5ad92a1c720a364eed87753adcfe7427ea4473f7
EleanorCordelia <bryan.rich0604@gmail.com>
2024-04-05 12:42:58 +0700

    feat: completing game config

--- src/GameConfig/GameConfig.hpp
+++ src/GameConfig/GameConfig.hpp
@@ -1,21 +1,61 @@
 #ifndef __GAME_CONFIG__
 #define __GAME_CONFIG__
 
+#include <iostream>
+#include <iomanip>
+
+/*
+* Game Configuration class.
+*/
 class GameConfig {
-    public:
+    private:
+        /*weight to win. from config file*/
         int weight_to_win;
+        /*amount of gulden to win. from config file*/
         int gulden_to_win;
+        /*inventory row from config file*/
         int inventory_row;
+        /*inventory column from config file*/
         int inventory_col;
+        /*pen row from config file*/
         int pen_row;
+        /*pen column from config file*/
         int pen_col;
+        /*field row from config file*/
         int field_row;
+        /*field column from config file*/
         int field_col;
-    
+
     public:
+        /*
+        * Default Constructor. initialize all variable to 0.
+        */
         GameConfig();
         
-        GameConfig(int, int, int, int, int, int, int, int);
+        /*
+        * User Defined Constructor. Initiate properties
+        * @param ww weight to win
+        * @param gw golden to win
+        * @param ir inventory row
+        * @param ic inventory col
+        * @param pr pen row
+        * @param pc pen col
+        * @param fr field row
+        * @param fc field col
+        */
+        GameConfig(int ww, int gw, int ir, int ic, int pr, int pc, int fr, int fc);
+
+        /*
+        * Copy Constructor.
+        * @param gc game config to copy
+        */
+        GameConfig(const GameConfig&);
+
+        GameConfig& operator=(const GameConfig&);
+
+        ~GameConfig();
+
+        friend std::ostream& operator<<(std::ostream& os, GameConfig gc);
 };
 
 #endif
