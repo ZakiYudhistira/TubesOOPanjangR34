fbdb656b69860eb49a8bcbde1ede64b9cfbb4d80
EleanorCordelia <bryan.rich0604@gmail.com>
2024-04-06 15:22:09 +0700

    feat: initiate omnivore

---
+++ src/Animal/Omnivore.cpp
@@ -0,0 +1,9 @@
+#include "Omnivore.hpp"
+
+Omnivore::Omnivore(): Animal() {
+    this->type = "";
+}
+
+Omnivore::Omnivore(int id, string code_name, string object_name, string type, int price, int weight_to_harvest): Animal(id, code_name, object_name, price, weight_to_harvest) {
+    this->type = type;
+}
