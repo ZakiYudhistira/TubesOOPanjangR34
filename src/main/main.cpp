b13396e7ef5a9e37c482bbf3071f0642828ee1c1
Akmal2205 <syarafiakml2004@gmail.com>
2024-04-08 16:32:35 +0700

    feat : main initialization

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -29,21 +29,23 @@ private:
     ProductConfig product_config;
     RecipeConfig recipe_config;
 
-    vector<Player*> player_list;
-    Player* current_player;
+    vector<Player *> player_list;
+    Player *current_player;
     int current = 0;
 
 public:
-    Main(){
+    Main()
+    {
         // input
         FileHandler file_scan;
-        try{
+        try
+        {
             file_scan.readFile("test/input/misc.txt", game_config);
             file_scan.readFile("test/input/animal.txt", animal_config);
             file_scan.readFile("test/input/plant.txt", plant_config);
             file_scan.readFile("test/input/product.txt", product_config);
             file_scan.readFile("test/input/recipe.txt", recipe_config);
-            
+
             // testing kalau MUAT
             file_scan.readFile("test/data/state.txt", player_list);
         }
@@ -53,32 +55,11 @@ public:
         }
     }
 
-    int indexOf(vector<string> strings, string name){
-        int idx = -1;
-        for (int i = 0; i < (int)strings.size(); i++)
-        {
-            if (strings[i] == name)
-            {
-                idx = i;
-            }
-        }
-        return idx;
-    }
-
-    void insertSorted(vector<string> &strings, string name)
+    void insertPlayer(Player *new_player)
     {
-        strings.push_back(name);
-        std::sort(strings.begin(), strings.end());
-    }
-
-    void insertPlayer(vector<Player> &list_of_player, vector<string> &player_leksikografis, Player new_player)
-    {
-        /*Memasukan nama player ke dalam player_queue sesuai urutan*/
-        insertSorted(player_leksikografis, new_player.getName());
-        /*Mencari index urutan dari player secara leksikografis (urutan nama)*/
-        int idx = indexOf(player_leksikografis, new_player.getName());
-        /*Memasukkan player ke dalam list_of_player sesuai index ururtan leksikografiknya*/
-        list_of_player.insert(list_of_player.begin() + idx, new_player);
+        this->player_list.push_back(new_player);
+        std::sort(this->player_list.begin(), this->player_list.end(), [](Player *lhs, Player *rhs)
+                  { return lhs->getName() < rhs->getName(); });
     }
 
     void main()
@@ -91,14 +72,20 @@ public:
         std::cout << recipe_config;
 
         /*Initialiazation (jika new game)*/
-        
+
         string walikota, petani, peternak;
         std::cout << "Input nama walikota : ";
         getline(std::cin, walikota);
+        Player *ptr_walikota = new Player(walikota, 40, 50);
+        insertPlayer(ptr_walikota);
         std::cout << "Input nama petani : ";
         getline(std::cin, petani);
+        Player *ptr_petani = new Player(petani, 40, 50);
+        insertPlayer(ptr_petani);
         std::cout << "Input nama peternak : ";
         getline(std::cin, peternak);
+        Player *ptr_peternak = new Player(peternak, 40, 50);
+        insertPlayer(ptr_peternak);
 
         /*Inisialisasi list leksikografis*/
         // vector<string> player_leksikografis;
@@ -110,13 +97,13 @@ public:
         //     player_list.push_back(Player(player_leksikografis[i], 40, 50));
         // }
 
-        // for (int i = 0; i < (int)player_leksikografis.size(); i++)
-        // {
-        //     std::cout << player_leksikografis[i] << "\n";
-        // }
+        for (int i = 0; i < (int)player_list.size(); i++)
+        {
+            std::cout << player_list[i]->getName() << "\n";
+        }
 
         /*Current player*/
-        
+
         // *current_player = player_list[current];
 
         /*Alur command current_player*/
@@ -134,7 +121,8 @@ public:
     }
 };
 
-int main(){
+int main()
+{
     Main m;
 
     m.main();
