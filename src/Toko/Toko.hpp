49a1fc865df65522f7289b1c7ae69258d63f70d3
WazeAzure <bryan.rich0604@gmail.com>
2024-04-10 20:02:14 +0700

    feat: update toko

--- src/Toko/Toko.hpp
+++ src/Toko/Toko.hpp
@@ -3,26 +3,37 @@
 
 #include <vector>
 #include <string>
+
+#include "Config/AnimalConfig.hpp"
+#include "Config/PlantConfig.hpp"
+#include "Config/ProductConfig.hpp"
+#include "Config/RecipeConfig.hpp"
+
 #include "GameObject/GameObject.hpp"
 
 using namespace std;
 
 class Toko {
     private:
-        int num_jenis_item;
-        vector<vector<GameObject, int>> items;
+        vector<pair<GameObject*, int>> item_list;
+        int neff;
+
     public:
         // Default Constructor
         Toko();
 
         // Constructor dengan item-item yang telah ditentukan
-        Toko(int num_jenis_item, vector<vector<GameObject, int>> items);
+        Toko(AnimalConfig& ac, PlantConfig& pc, ProductConfig& prod, RecipeConfig& rc);
+
+        Toko(Toko& t);
+
+        Toko& operator=(Toko&);
 
         // Getter banyak jenis Item yang tersedia di toko
         int getNumJenisItem();
 
         // Operator overload untuk output item pada toko
-        friend ostream operator<<(ostream& os, Toko t);
+        friend ostream& operator<<(ostream& os, Toko t);
 
         // Melakukan pembelian dari seorang player pada toko
         void beli();
